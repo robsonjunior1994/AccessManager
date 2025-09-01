9ef4af2f18fb05eed5cf8b955dd5d6f2f1a075a7
Robson Junior <robsonjunior1994@gmail.com>
2025-09-01 17:44:22 -0300

    formatando classe.

--- AccessManager.Api/Common/Result.cs
+++ AccessManager.Api/Common/Result.cs
@@ -7,7 +7,11 @@
         public string ErrorMessage { get; set; }
         public ErrorCode ErrorCode { get; set; }
 
-        public static Result<T> Success(T data) => new() { IsSuccess = true, Data = data };
+        public static Result<T> Success(T data) => new() 
+        {   
+            IsSuccess = true, 
+            Data = data 
+        };
 
         public static Result<T> Failure(string errorMessage, ErrorCode errorCode) => new()
         {
